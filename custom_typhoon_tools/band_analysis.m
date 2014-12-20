596cf299696d31966fb743282b0106e9cfff9b7d
jonasfunke <jonasfunke@gmail.com>
2014-12-20 21:18:06 +0100

    check_gel_saturation

--- custom_typhoon_tools/band_analysis.m
+++ custom_typhoon_tools/band_analysis.m
@@ -4,6 +4,9 @@ close all, clear all, clc
 %% load gel data
 gelData_raw = load_gel_image('data_dir', data_directory);
 
+%%
+[gelData_raw, cf] = check_gel_saturation(gelData_raw);
+
 %% background correct data
 gelData = background_correct_gel_image(gelData_raw, 'numberOfAreas', 4);
 
