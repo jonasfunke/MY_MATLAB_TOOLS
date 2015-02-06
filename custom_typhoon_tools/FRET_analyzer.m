1eea2892fd07fe25c2a233bc601a1bb7348e34f2
jonasfunke <jonasfunke@gmail.com>
2015-02-06 14:43:19 +0100

    fret analyzer

--- custom_typhoon_tools/FRET_analyzer.m
+++ custom_typhoon_tools/FRET_analyzer.m
@@ -17,25 +17,105 @@ prefix_out = tmp{1};
 path_out = [gelData.pathnames{1} prefix_out filesep];
 mkdir(path_out);
 
-
 %% shift images
 %[dd_bg, da_x_min, da_y_min] = overlay_image(da_bg, dd_bg, 10);
 %[aa_bg, aa_x_min, aa_y_min]= overlay_image(da_bg, aa_bg, 10);
 
 %% leakage and direct-excitation correction factors
-leak_dir  = calculate_corrections(gelData.images{1}, gelData.images{3}, gelData.images{2}, [path_out filesep prefix_out '_correction.txt']);
-da_cor = gelData.images{3} - leak_dir(1,1).*gelData.images{1} - leak_dir(2,1).*gelData.images{2};%-leak_dir(1,2)-leak_dir(2,2); 
+[leak_dir, leakdir_fig]  = calculate_leakage_directexcitation(gelData.images{1}, gelData.images{3}, gelData.images{2}, 'display', 'on');
+print(leakdir_fig, '-dtiff', '-r 500' , [path_out filesep 'Leakage_DirEx_correction.tif']); %save figure
+% wrtite leakdir
+save([path_out filesep 'Leakage_DirEx_data.txt'], 'leak_dir' ,'-ascii')
+
+da_cor = gelData.images{3} - leak_dir(1,1).*gelData.images{1} - leak_dir(2,1).*gelData.images{2};%-leak_dir(1,2)-leak_dir(2,2); %correct image
+gelData.images = [gelData.images; da_cor]; % append to images
 
 %% integrate bands
 bandData = get_band_intensities(gelData);
 
+%% calculate intensities based on ratio
+%{
+I = zeros(18,3);
+for i=1:18
+    pos = bandData.positions(i,:);
+    subDD = gelData.images{1}( pos(2):pos(2)+pos(4),pos(1):pos(1)+pos(3) );
+    subAA = gelData.images{2}( pos(2):pos(2)+pos(4),pos(1):pos(1)+pos(3) );
+    subDA = gelData.images{4}( pos(2):pos(2)+pos(4),pos(1):pos(1)+pos(3) );
+    
+    shiftDA = min(min(min(subDA))+0.000000001,0.000000001)
+    shiftDD = min(min(min(subDD))+0.000000001,0.000000001)
+        shiftAA = min(min(min(subAA))+0.000000001,0.000000001)
+
+    E3(i) = sum(sum(( (subAA-shiftAA)./ sum(subAA(:)-shiftAA)).* (subDA-shiftDA) ./ ((subDA-shiftDA) + gamma_calc.*(subDD-shiftDD)))) ;
+    
+    bla =  (subDA-shiftDA) ./ ((subDA-shiftDA) + gamma_calc.*(subDD-shiftDD));
+    prob = (subAA-shiftAA)./ sum(subAA(:)-shiftAA);
+    [n, x] = histwc(bla(:), prob(:), 300);
+    figure
+    bar(x, n)
+    title([num2str(i) ' weighted'])
+
+    %figure
+    %hist(bla(:), 100)
+    
+    %imagesc(   (subAA./ sum(subAA(:))).* (subDA+shiftDA) ./ ((subDA+shiftDA) + gamma_calc.*(subDD+shiftDD))) , colormap gray, axis image
+    
+    %pause
+    p = calculate_ration_of_areas(subDD, subAA, 'display', 'off')
+    I(i,1) = p(1);   
+   % title(num2str(i))
+    p = calculate_ration_of_areas(subDA, subAA, 'display', 'off')
+    I(i,2) = p(1);
+      %  title(num2str(i))
+
+    p = calculate_ration_of_areas(subDD, subDA, 'display', 'off')
+    I(i,3) = p(1);
+      %  title(num2str(i))
+
+end
+
+%}
 %%
-gamma =0.1;
-gamma_calc =  bandData.intensities(end,3).*(1./0.5 - 1) ./  bandData.intensities(end,1) 
-E = bandData.intensities(:,3) ./ (gamma_calc.*bandData.intensities(:,1) + bandData.intensities(:,3));
+gamma_calc =  bandData.intensities(end,4).*(1./0.5 - 1) ./  bandData.intensities(end,1) 
+E = bandData.intensities(:,4) ./ (gamma_calc.*bandData.intensities(:,1) + bandData.intensities(:,4));
+%E_ratio = I(:,2) ./ (I(:,2) + gamma_calc.*I(:,1));
+%E_ratio2 = 1 ./ (1 + gamma_calc.*I(:,3));
+
 n_bands = size(bandData.intensities,1);
-close all
-plot(1:n_bands, E, 'b.-')
+%close all
+figure
+plot(1:n_bands, E, 'r.-')
+
+
+
+%% save data
+save([path_out prefix_out '_data.mat'])
+disp('data saved...')
+
+%% write corrected images
+disp('Writing images')
+
+t = Tiff([path_out filesep 'da_cor.tif'],'w');
+t.setTag('Photometric',Tiff.Photometric.MinIsWhite);
+t.setTag('BitsPerSample',16);
+t.setTag('SampleFormat',Tiff.SampleFormat.UInt);
+t.setTag('ImageLength',size(da_cor,1));
+t.setTag('ImageWidth',size(da_cor,2));
+t.setTag('SamplesPerPixel',1);
+t.setTag('PlanarConfiguration',Tiff.PlanarConfiguration.Chunky);
+t.write( uint16(da_cor-min(da_cor(:)))  );
+t.close();
+
+t = Tiff([path_out filesep 'da_cor+bg.tif'],'w');
+t.setTag('Photometric',Tiff.Photometric.MinIsWhite);
+t.setTag('BitsPerSample',16);
+t.setTag('SampleFormat',Tiff.SampleFormat.UInt);
+t.setTag('ImageLength',size(da_cor,1));
+t.setTag('ImageWidth',size(da_cor,2));
+t.setTag('SamplesPerPixel',1);
+t.setTag('PlanarConfiguration',Tiff.PlanarConfiguration.Chunky);
+t.write( uint16(da_cor+gelData.background{3}.p00)  );
+t.close();
 
 
 %%
@@ -100,34 +180,6 @@ ylabel('Normalized FRET efficiency')
 print(cur_fig, '-dtiff', '-r 500' , [path_out filesep prefix_out '_fret_conc.tif']); %save figure
 
 
-%% save data
-save([path_out prefix_out '_data.mat'])
-
-
-%% write corrected images
-disp('Writing images')
-
-t = Tiff([path_out filesep 'da_cor.tif'],'w');
-t.setTag('Photometric',Tiff.Photometric.MinIsWhite);
-t.setTag('BitsPerSample',16);
-t.setTag('SampleFormat',Tiff.SampleFormat.UInt);
-t.setTag('ImageLength',size(da_cor,1));
-t.setTag('ImageWidth',size(da_cor,2));
-t.setTag('SamplesPerPixel',1);
-t.setTag('PlanarConfiguration',Tiff.PlanarConfiguration.Chunky);
-t.write( uint16(da_cor-min(da_cor(:)))  );
-t.close();
-
-t = Tiff([path_out filesep 'da_cor+bg.tif'],'w');
-t.setTag('Photometric',Tiff.Photometric.MinIsWhite);
-t.setTag('BitsPerSample',16);
-t.setTag('SampleFormat',Tiff.SampleFormat.UInt);
-t.setTag('ImageLength',size(da_cor,1));
-t.setTag('ImageWidth',size(da_cor,2));
-t.setTag('SamplesPerPixel',1);
-t.setTag('PlanarConfiguration',Tiff.PlanarConfiguration.Chunky);
-t.write( uint16(da_cor+gelData.background{3}.p00)  );
-t.close();
 
 
 
