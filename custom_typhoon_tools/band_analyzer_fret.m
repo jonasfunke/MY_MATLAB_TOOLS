2f4ccfb53548c420240e3c7dba2cfaa96f566c9a
jonasfunke <jonasfunke@gmail.com>
2015-02-07 15:36:30 +0100

    band_analyzer_fret

--- custom_typhoon_tools/band_analyzer_fret.m
+++ custom_typhoon_tools/band_analyzer_fret.m
@@ -4,11 +4,11 @@ close all, clear all, clc
 %% load gel data
 gelData_raw = load_gel_image('data_dir', data_directory);
 
+%% check for saturation
+gelData_raw = check_gel_saturation(gelData_raw);
+
 %% background correct data
 gelData = background_correct_gel_image(gelData_raw, 'numberOfAreas', 4);
-
-%% check for saturation
-gelData = check_gel_saturation(gelData);
 
 %% create output dir
 prefix_out = [gelData.filenames{1}(1:end-4) '_analysis_' datestr(now, 'yyyy-mm-dd_HH-MM')];
@@ -24,11 +24,12 @@ mkdir(path_out);
 %% leakage and direct-excitation correction factors
 [leak_dir, leakdir_fig]  = calculate_leakage_directexcitation(gelData.images{1}, gelData.images{3}, gelData.images{2}, 'display', 'on');
 print(leakdir_fig, '-dtiff', '-r 500' , [path_out filesep 'Leakage_DirEx_correction.tif']); %save figure
-% wrtite leakdir
+
+% write leakdir
 save([path_out filesep 'Leakage_DirEx_data.txt'], 'leak_dir' ,'-ascii')
 
 da_cor = gelData.images{3} - leak_dir(1,1).*gelData.images{1} - leak_dir(2,1).*gelData.images{2};%-leak_dir(1,2)-leak_dir(2,2); %correct image
-gelData.images = [gelData.images; da_cor]; % append to images
+gelData.images{4} = da_cor; % append to images
 
 %% integrate bands
 bandData = get_band_intensities(gelData);
@@ -75,20 +76,14 @@ for i=1:18
 end
 
 %}
+
 %%
 gamma_calc =  bandData.intensities(end,4).*(1./0.5 - 1) ./  bandData.intensities(end,1) 
 E = bandData.intensities(:,4) ./ (gamma_calc.*bandData.intensities(:,1) + bandData.intensities(:,4));
-%E_ratio = I(:,2) ./ (I(:,2) + gamma_calc.*I(:,1));
-%E_ratio2 = 1 ./ (1 + gamma_calc.*I(:,3));
-
-n_bands = size(bandData.intensities,1);
-%close all
-figure
-plot(1:n_bands, E, 'r.-')
-
 
 
 %% save data
+close all
 save([path_out prefix_out '_data.mat'])
 disp('data saved...')
 
@@ -117,70 +112,23 @@ t.setTag('PlanarConfiguration',Tiff.PlanarConfiguration.Chunky);
 t.write( uint16(da_cor+gelData.background{3}.p00)  );
 t.close();
 
+%% 
+disp('Done.')
 
-%%
-scrsz = get(0,'screensize');
-fig_dim =[15 10];
-cur_fig = figure('Visible','on', 'PaperPositionMode', 'manual','PaperUnits','centimeters','PaperPosition', [0 0 fig_dim(1) fig_dim(2)], 'Position', [0 scrsz(4) fig_dim(1)*40 fig_dim(2)*40]);
+%% Plot 
 
-subplot(1,2,1)
-plot(1:n_bands, E, 'b.-', 1:n_bands, bla.E, 'r.-')
-legend({'no-stack: 2001', 'stack: 0021'})
-set(gca, 'YLim', [0.2 0.32], 'XLim', [0 20])
-xlabel('Lane')
-ylabel('Raw FRET efficiency')
+n_bands = size(bandData.intensities,1);
+cur_fig = figure;
+subplot(3, 1, 1)
+%plot(1:n_bands, bandData.intensities(:,2), 'r.-', 1:n_bands, bandData.intensities(:,1), 'g.-', 1:n_bands, bandData.intensities(:,4), 'b.-')
+plot( 1:n_bands, gamma_calc.*bandData.intensities(:,1)./bandData.intensities(:,2), 'g.-', 1:n_bands, bandData.intensities(:,4)./bandData.intensities(:,2), 'b.-')
+xlabel('Lane'), ylabel('Normalized bandintensity')
+legend({'gamma * D->D / A->A', 'D->A / A->A'}, 'location', 'best')
 
-subplot(1,2,2)
-plot(c, E(1:end-1),'b.-', c,  bla.E(1:end-1), 'r.-')
-legend({'no-stack: 2001', 'stack: 0021'})
-set(gca, 'YLim',  [0.2 0.32], 'XLim', [0 2])
-xlabel('Additional NaCl [M]')
-ylabel('Raw FRET efficiency')
+subplot(3, 1, 2:3)
+plot(1:n_bands, E, 'k.-')
+xlabel('Lane'), ylabel('FRET efficiency')
 
-print(cur_fig, '-dtiff', '-r 500' , [path_out filesep prefix_out '_raw-fret.tif']); %save figure
+legend({['gamma=' num2str(gamma_calc) ]})
 
-%%
-close all
-
-y1 = mean(E(1:5));
-x1 = mean(bla.E(1:5));
-y2 = E(end);
-x2 = bla.E(end);
-a = (y2-y1)./ (x2-x1)
-b = (x1*y2-x2*y1) / (x1-x2)
-plot(1:n_bands, E, 'b.-', 1:n_bands, bla.E.*a+b, 'r.-')
-
-
-
-%%
-E1 = E-mean(E(16:17));
-E1 = E1/ median(E1(1:5));
-E2 = bla.E-mean(bla.E(16:17));
-E2 = E2/ median(E2(1:5));
-
-
-%%
-cur_fig = figure();
-plot(1:n_bands, E1, 'b.-', 1:n_bands, E2, 'r.-')
-legend({'no-stack: 2001', 'stack: 0021'})
-set(gca, 'YLim', [-0.1 1.2], 'XLim', [0 20])
-xlabel('Lane')
-ylabel('Normalized FRET efficiency')
-
-print(cur_fig, '-dtiff', '-r 500' , [path_out filesep prefix_out '_fret.tif']); %save figure
-
-%%
-cur_fig = figure();
-plot(c, E1(1:end-1), 'b.-', c, E2(1:end-1), 'r.-')
-legend({'no-stack: 2001', 'stack: 0021'})
-set(gca, 'YLim', [-0.1 1.2], 'XLim', [0 2])
-xlabel('Additional NaCl [M]')
-ylabel('Normalized FRET efficiency')
-
-print(cur_fig, '-dtiff', '-r 500' , [path_out filesep prefix_out '_fret_conc.tif']); %save figure
-
-
-
-
-
-%%
+print(cur_fig, '-dtiff', '-r 500' , [path_out filesep 'FRET_normalized.tif']); %save figure
