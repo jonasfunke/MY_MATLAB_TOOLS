90826ff2280eb20bde9249a597913e96d2e72d30
jonasfunke <jonasfunke@gmail.com>
2015-07-30 12:00:59 +0200

    classification_tools

--- custom_tem_tools/classification_analysis.m
+++ custom_tem_tools/classification_analysis.m
@@ -1,22 +1,81 @@
 %%
 close all, clear all, clc
 
-load('/Users/jonasfunke/Dropbox/POSITIONING/Figure_1/angle_calibration_02/data/average_angles.mat')
+
 load('/Volumes/matlabuser/jonasfunke/data/NoInt_FoB25/2015-07-27_14-00_automatic_classification/data_images_all.mat')
-
+load('/Users/jonasfunke/Dropbox/POSITIONING/Figure_1/angle_calibration_02/data/average_angles.mat')
 no_int = load('/Users/jonasfunke/Dropbox/Nucleosome_Dropbox/Figure4/TEM/Angle_references/no_interaction/iter940/Mean_angles_no_int.txt');
+
+
+
+%load('/Volumes/matlabuser/jonasfunke/data/NoInt_FoB25/2015-07-27_14-00_automatic_classification/data_images_all.mat')
+
+% load('/Volumes/matlabuser/jonasfunke/data/NoInt_FoB25/2015-07-28_15-08_automatic_classification/data_before_150531_PK_GR_13__B2_FRETst_noint_stack.mat')
+% load('/Volumes/matlabuser/jonasfunke/data/NoInt_FoB25/2015-07-28_15-08_automatic_classification/data_images_all.mat')
+
+load('/Volumes/matlabuser/jonasfunke/data/inner_0deg_Fob25/2015-07-29_09-19_automatic_classification/data_image1-1280.mat')
+load('/Volumes/matlabuser/jonasfunke/data/inner_0deg_Fob25/2015-07-29_09-19_automatic_classification/data_before_images_export.mat')
+
+
+%% correct classification
+
+alpha = 0:dalpha:359;  % angles
+n_rot = length(alpha); % number of rotations
+classification_cor = zeros(size(classification,1), 5);
+cor_matrix_cor = cell(size(cor_matrix));
+
+for i=1:1280 % loop through images
+
+    % determine best rotation
+    tmp =min(cor_matrix{i});
+    bg = tmp(ones(2*n_rot,1),:);
+    cor_matrix_cor{i} = cor_matrix{i}-bg;
+    [max_cor, i_tmp] = max(cor_matrix{i}(:)-bg(:)); % use best overall correlation
+    [best_rot, best_ref] = ind2sub(size(cor_matrix{i}),i_tmp);
+        
+    mirror = best_rot > n_rot; % determine if image was mirrored
+    % determine rotation angle
+    if mirror 
+        alpha_found = alpha(best_rot-n_rot);
+    else
+        alpha_found = alpha(best_rot);
+    end
+    classification_cor(i,:) = [i best_ref alpha_found mirror max_cor]; % combine output
+    
+end 
+
+
 %%
-tmp = angle(1:2:end,1);
-xhist = 1:38;
-n = hist(classification(:,2), xhist);
+alpha_ref = [angle(1:end,1); no_int(:,2)];
 
+n_hist = zeros(1,N_ref);
+for i=1:N_ref
+    n_hist(i) = sum(classification_cor(:,2)==i); % count how many images fall into class i
+end
+
+%%
 figure(1)
-bar(xhist, n)
-
+plot(n_hist, '.')
 figure(2)
-alpha = [angle(1:2:end,1); no_int(:,2)];
+bar(alpha_ref, n_hist)
+figure(3)
+plot(alpha_ref, 1:N_ref, '.')
 
-bar(alpha, n)
+
+%%
+tmp = [alpha_ref n_hist'];
+
+tmp = sortrows(tmp, 1);
+
+x = tmp(1:end-1,1)+diff(tmp(:,1))/2;
+
+width = diff(x);
+%
+bla = tmp(2:end-1,2) ./ width;
+close all
+area(tmp(:,1), tmp(:,2)), hold on
+%plot(tmp(2:end-1,1) , bla, '.-')
+
 
 
 
--- custom_tem_tools/classification_plot.m
+++ custom_tem_tools/classification_plot.m
@@ -4,73 +4,115 @@
 
 % load classification file
 
+% 
+% for i=100:200%N_img % loop through images
+% 
+%     figure(1)
+%     subplot(1,4,1)
+%     imagesc(img(:,:,i)), axis image
+%     title(['Original image: ' num2str(i)]  )
+%  
+%     subplot(1,4,2)
+%     rot_found = my_imagetransform(img(:,:,i), classification(i,3), classification(i,2)); % mirror, angle
+%     imagesc(rot_found), axis image
+%     title('Rotated image')
+%     
+%     subplot(1,4,3)
+%     imagesc(ref_filtered(:,:,classification(i,1))), axis image
+%     title(['Reference ' num2str(classification(i,1))])
+% 
+%     x_cor = normxcorr2(ref_filtered(:,:,classification(i,1)), rot_found) ;
+%     x_cor = x_cor(100:300,100:300);
+%     subplot(1,4,4)
+%     hold off
+%     imagesc(x_cor ), axis image, hold on
+%     [tmp_cor, i_tmp] = max(x_cor(:)); % use best overall correlation
+%     [best_i, best_j] = ind2sub(size(x_cor),i_tmp);
+%     plot(best_j, best_i, 'ro')
+%     title(['x-correlation image (cor = ' num2str(tmp_cor) ')'])
+%     
+%     figure(2)
+%     hold off
+%     imagesc(cor_matrix{i}), axis image, colorbar, hold on
+%  
+%     [~, i_tmp] = max(cor_matrix{i}(:)); % use best overall correlation
+%     [best_rot, best_ref] = ind2sub(size(cor_matrix{i}),i_tmp);
+%     plot(best_ref, best_rot, 'ro')
+%    
+%     figure(3)
+%     subplot(1,2,1)
+%     hold off
+%     tmp =min(cor_matrix{i});
+%     bg = tmp(ones(144,1),:);
+%     imagesc(cor_matrix{i}-bg), axis image, colorbar, hold on
+%     
+%     [~, i_tmp] = max(cor_matrix{i}(:)-bg(:)); % use best overall correlation
+%     [best_rot, best_ref] = ind2sub(size(cor_matrix{i}),i_tmp);
+%     plot(best_ref, best_rot, 'ro')
+%     
+%     subplot(1,2,2)
+%     imagesc(ref_filtered(:,:,best_ref)) , axis image
+%     
+%     
+%    pause
+% end
+% 
 
-for i=1:100%N_img % loop through images
+%% correct classification for background
 
-    figure(1)
-    subplot(1,4,1)
-    imagesc(img(:,:,i)), axis image
-    title(['Original image: ' num2str(i)]  )
- 
-    subplot(1,4,2)
-    rot_found = my_imagetransform(img(:,:,i), classification(i,3), classification(i,2)); % mirror, angle
-    imagesc(rot_found), axis image
-    title('Rotated image')
-    
-    subplot(1,4,3)
-    imagesc(ref_filtered(:,:,classification(i,1))), axis image
-    title(['Reference ' num2str(classification(i,1))])
+alpha = 0:dalpha:359;  % angles
+n_rot = length(alpha); % number of rotations
+classification_cor = zeros(size(classification,1), 4);
+cor_matrix_cor = cell(size(cor_matrix));
 
-    x_cor = normxcorr2(ref_filtered(:,:,classification(i,1)), rot_found) ;
-    x_cor = x_cor(100:300,100:300);
-    subplot(1,4,4)
-    hold off
-    imagesc(x_cor ), axis image, hold on
-    [tmp_cor, i_tmp] = max(x_cor(:)); % use best overall correlation
-    [best_i, best_j] = ind2sub(size(x_cor),i_tmp);
-    plot(best_j, best_i, 'ro')
-    title(['x-correlation image (cor = ' num2str(tmp_cor) ')'])
-    
-    figure(2)
-    hold off
-    imagesc(cor_matrix{i}), axis image, colorbar, hold on
- 
-    [~, i_tmp] = max(cor_matrix{i}(:)); % use best overall correlation
-    [best_rot, best_ref] = ind2sub(size(cor_matrix{i}),i_tmp);
-    plot(best_ref, best_rot, 'ro')
-   
-    figure(3)
-    subplot(1,2,1)
-    hold off
+for i=1:1000 % loop through images
+
+    % determine best rotation
     tmp =min(cor_matrix{i});
-    bg = tmp(ones(144,1),:);
-    imagesc(cor_matrix{i}-bg), axis image, colorbar, hold on
-    
-    [~, i_tmp] = max(cor_matrix{i}(:)-bg(:)); % use best overall correlation
+    bg = tmp(ones(2*n_rot,1),:);
+    cor_matrix_cor{i} = cor_matrix{i}-bg;
+    [max_cor, i_tmp] = max(cor_matrix{i}(:)-bg(:)); % use best overall correlation
     [best_rot, best_ref] = ind2sub(size(cor_matrix{i}),i_tmp);
-    plot(best_ref, best_rot, 'ro')
+        
+    mirror = best_rot > n_rot; % determine if image was mirrored
+    % determine rotation angle
+    if mirror 
+        alpha_found = alpha(best_rot-n_rot);
+    else
+        alpha_found = alpha(best_rot);
+    end
+    classification_cor(i,:) = [best_ref alpha_found mirror max_cor]; % combine output
     
-    subplot(1,2,2)
-    imagesc(ref_filtered(:,:,best_ref)) , axis image
+end 
+
+
+
+for i=1:200%N_img % loop through images
+    figure(1)
     
+    subplot(2,2,1)
+    rot_found = my_imagetransform(img(:,:,i), classification_cor(i,3), classification_cor(i,2)); % mirror, angle
+    imagesc(rot_found), axis image
+    title(['Rotated image ' num2str(i)])
     
+    subplot(2,2,2)
+    imagesc(ref_filtered(:,:,classification_cor(i,1))), axis image
+    title(['Reference ' num2str(classification_cor(i,1)) ', (cor = ' num2str(tmp_cor) ')'])
+
+   subplot(2,2,[3 4])
+
+    hold off
+    imagesc(cor_matrix_cor{i}'), axis image, colorbar, hold on
+    
+    [~, i_tmp] = max(cor_matrix_cor{i}(:)); % use best overall correlation
+    [best_rot, best_ref] = ind2sub(size(cor_matrix_cor{i}),i_tmp);
+    plot( best_rot,best_ref, 'ro')
+   
+    xlabel('Rotation'), ylabel('Reference')
+    title('Maximum correlation')
    pause
 end
 
-
-%%
-
-
-close all
-
-cx=100;cy=100;ix=200;iy=200; r=101;
-tic
-[x,y]=meshgrid(-(cx-1):(ix-cx),-(cy-1):(iy-cy));
-c_mask=((x.^2+y.^2)<=r^2);
-bla = c_mask.*img(:,:,1);
-toc
-
-imagesc(bla), colorbar
 
 
 
