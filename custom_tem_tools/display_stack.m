20c73143add89abb61c198287ba6bebc68fe1542
jonasfunke <jonasfunke@gmail.com>
2015-10-03 18:51:55 +0200

    display_stack update

--- custom_tem_tools/display_stack.m
+++ custom_tem_tools/display_stack.m
@@ -1,36 +1,56 @@
-%% refine 
-disp('Refining particles...')
+function [ index_out ] = display_stack( varargin )
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
 
-limit = zeros(n_ref/(mirror+1), 1);
-if refine
-    for t=1:n_ref/(mirror+1)
-        [cc_sort, sort_index] = sortrows(data(t).stats(:,3), -1);
-        i = [1:size(cc_sort,1)]';
-
-        close all
-        subplot(1, 2, 1)
-        imagesc(data(t).particles_rot(:,:,sort_index(1))), axis image, colormap gray
-       % title(['Ref ' num2str(t) ', particle ' num2str(i) ', cc = ' num2str(data(t).stats(sort_index(1),3))])
-        cur_img = gca;
-
-        subplot(1, 2, 2)
-        plot(i, cc_sort, 'b'), hold on
-        ylim = [0 1];
-        set(gca, 'YLim', ylim, 'XLim', [1 i(end)]);
-        h = imline(gca,[1 1], ylim);
-        setColor(h,[1 0 0]);
-        setPositionConstraintFcn(h, @(pos)[ min( i(end), max(1,[pos(2,1);pos(2,1)])) ylim'   ])
-
-        id = addNewPositionCallback(h, @(pos) update_img(  data(t).particles_rot(:,:,sort_index(  max(1, min(i(end), round(pos(1,1))))   ) ), cur_img )  );
-        id2 = addNewPositionCallback(h, @(pos) title(['cc = ' num2str( cc_sort(  max(1, min(i(end), round(pos(1,1))))) )]) );
-
-        pos_line = wait(h);
-        limit_index = max(1, min(i(end), round(pos_line(1,1))));
-        limit(t) = cc_sort(limit_index);
-
+    if isempty(varargin)
+        
+        [fname, pname] = uigetfile('*.mat', 'Select a stack (.mat file)');
+        data = load([pname fname]);
+        
+    else
+        data = varargin{1};
     end
-    pause(0.1)
-    close all
+    dx = 4;
+    xhist = 0:dx:120;
+    n = hist(data.angles(:), xhist);
+    
+    [angles_sorted, sort_index] = sortrows(data.angles, +1);
+    i = [1:size(data.angles,1)]';
+
+    cf = figure();
+    
+    subplot(1, 4, 1:2)
+    imagesc(data.particles(:,:,sort_index(1))), axis image, colormap gray
+   % title(['Ref ' num2str(t) ', particle ' num2str(i) ', cc = ' num2str(data(t).stats(sort_index(1),3))])
+    cur_img = gca;
+    
+    subplot(1, 4, 4)
+    ylim = [min(data.angles) max(data.angles)];    
+    plot(n, xhist)
+    set(gca, 'Ylim', ylim)
+
+    grid on
+
+    subplot(1, 4, 3)
+    plot(i, angles_sorted, 'b'), hold on
+    set(gca, 'YLim', ylim, 'XLim', [1 i(end)]);
+    h = imline(gca, round(length(data.angles)/2).*[1 1], ylim);
+    setColor(h,[1 0 0]);
+    setPositionConstraintFcn(h, @(pos)[ min( i(end), max(1,[pos(2,1);pos(2,1)])) ylim'   ])
+
+    id = addNewPositionCallback(h, @(pos) update_img(  data.particles(:,:,sort_index(  max(1, min(i(end), round(pos(1,1))))   ) ), cur_img )  );
+    id2 = addNewPositionCallback(h, @(pos) title(['Angle = ' num2str( angles_sorted(  max(1, min(i(end), round(pos(1,1))))) )]) );
+    grid on
+
+    pos_line = wait(h);
+    index_selected = max(1, min(i(end), round(pos_line(1,1))));
+    
+    index_out = sort_index(index_selected);
+    pause(0.01)
+    close(cf)
+
+
+
+
 end
-pause(0.1)
-close all
+
