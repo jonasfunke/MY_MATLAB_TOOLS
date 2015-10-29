1d35a8e58fac11c0f05a9f24f13feb882f756b72
jonasfunke <jonasfunke@gmail.com>
2015-10-29 10:58:21 +0100

    added blind_sort

---
+++ custom_tem_tools/blind_sort_stack.m
@@ -0,0 +1,82 @@
+function [ data_out ] = blind_sort_stack( data_array )
+%
+%   Detailed explanation goes here
+
+
+    % compine data sets
+    all_angles = zeros(0,3);
+    for i=1:length(data_array)
+        all_angles = [all_angles; data_array(i).angles, [1:length(data_array(i).angles)]', i.*ones(length(data_array(i).angles),1)];
+    end
+    
+    %
+    N_particles = size(all_angles,1);
+    p = randperm(N_particles)';
+    use = zeros(N_particles, 1);
+    
+    % loop through mixed data set
+    cf = figure(1);
+    go_on = 1;
+    i = 1;
+    while go_on
+        j = p(i); % consider j-th particle
+
+        imagesc( data_array(all_angles(j,3)).particles(:,:,all_angles(j,2)) ), axis image, colormap gray
+        title(['Image ' num2str(i) ' of ' num2str(N_particles) ', angle = ' num2str(all_angles(j,1)) ])
+
+        k=waitforbuttonpress;
+        tmp = get(gcf,'currentcharacter');
+        if strcmp(tmp, 'd')
+            use(j) = 0;
+            disp([num2str(j) ' dismissed'])    
+        else
+            if strcmp(tmp, 'b')
+                i = i-2;
+                disp('back')
+            else
+                if strcmp(tmp, 'q')
+                     go_on = 0;
+                     disp('Quit')
+                else
+                   % if strcmp(tmp, 'w')
+                        %save([pname fname(1:end-4) '_sorting_1-' num2str(i) '.mat'], 'use')
+                        %disp('data written')
+                   % else
+                        disp([num2str(j) ' used'])
+                        use(j) = 1;
+                   % end
+                end
+            end
+        end
+        pause(0.01)
+
+        if i==N_particles
+            go_on = 0;
+        else
+            i=i+1;
+        end
+    end
+    
+    used_angles = all_angles(use==1,:);    
+    data_out = data_array; % copy struct
+    for i=1:length(data_array)
+        data_out(i).angles = used_angles(used_angles(:,3)==i,1);
+        data_out(i).particles =  data_array(i).particles(:,:, used_angles(used_angles(:,3)==i, 2));
+   
+        if isempty(data_array(i).history)
+            data_out(i).history = {['sorted on ' datestr(now, 'yyyy-mm-dd_HH-MM')]};
+        else
+            data_out(i).history = [data_out(i).history; {['sorted on ' datestr(now, 'yyyy-mm-dd_HH-MM')]}];
+        end
+        message = [num2str(length(data_array(i).angles)-length(data_out(i).angles)) ...
+            ' of ' num2str(length(data_array(i).angles)) ' particles dismissed. ' num2str(length(data_out(i).angles)) ' particles remain.'];
+
+        data_out(i).history = [data_out(i).history; {message}];
+        disp(message)
+    end
+
+    close(cf)
+    
+
+end
+
---
+++ custom_tem_tools/sort_stack.m
@@ -0,0 +1,75 @@
+function [ data_out ] = sort_stack( varargin )
+%UNTITLED6 Summary of this function goes here
+%   Detailed explanation goes here
+
+    if isempty(varargin)
+        [fname, pname] = uigetfile('*.mat', 'Select a stack (.mat file)');
+        data = load([pname fname]);
+    else
+        data = varargin{1};
+      %  pname = cd;
+    end
+    
+    N_particles = size(data.particles,3);
+    use = zeros(N_particles, 1);
+    
+    cf = figure(1);
+    go_on = 1;
+    i = 1;
+    while go_on
+        j = i; % consider j-th particle
+
+        imagesc(data.particles(:,:, j)), axis image, colormap gray
+        title(['Image ' num2str(i) ' of ' num2str(N_particles)  ])
+
+        k=waitforbuttonpress;
+        tmp = get(gcf,'currentcharacter');
+        if strcmp(tmp, 'd')
+            use(j) = 0;
+            disp([num2str(j) ' dismissed'])    
+        else
+            if strcmp(tmp, 'b')
+                i = i-2;
+                disp('back')
+            else
+                if strcmp(tmp, 'q')
+                     go_on = 0;
+                     disp('Quit')
+                else
+                   % if strcmp(tmp, 'w')
+                        %save([pname fname(1:end-4) '_sorting_1-' num2str(i) '.mat'], 'use')
+                        %disp('data written')
+                   % else
+                        disp([num2str(j) ' used'])
+                        use(j) = 1;
+                   % end
+                end
+            end
+        end
+        pause(0.01)
+
+        if i==N_particles
+            go_on = 0;
+        else
+            i=i+1;
+        end
+    end
+    
+    data_out = data; % copy struct
+    data_out.angles = data.angles(use==1);
+    data_out.particles = data.particles(:,:,use==1);
+    if isempty(data.history)
+        data_out.history = {['sorted on ' datestr(now, 'yyyy-mm-dd_HH-MM')]};
+    else
+        data_out.history = [data_out.history; {['sorted on ' datestr(now, 'yyyy-mm-dd_HH-MM')]}];
+    end
+    message = [num2str(N_particles-sum(use)) ' of ' num2str(N_particles) ' particles dismissed. ' num2str(sum(use)) ' particles remain.'];
+    
+    data_out.history = [data_out.history; {message}];
+    disp(message)
+
+    close(cf)
+    
+
+end
+
