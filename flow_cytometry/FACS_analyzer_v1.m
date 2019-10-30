6ae2d8f921a8aef7fa9240c4bb64da8dcd809311
jonasfunke <jonasfunke@gmail.com>
2019-10-30 16:53:42 +0100

    Update FACS_analyzer_v1.m

--- flow_cytometry/FACS_analyzer_v1.m
+++ flow_cytometry/FACS_analyzer_v1.m
@@ -7,27 +7,23 @@ prefix_out = [ datestr(now, 'yyyy-mm-dd_HH-MM') '_analysis'];
 path_out = [pathname prefix_out filesep];
 mkdir(path_out);
 
-
-%%
-
+%% make a cell array even if you have one file only
 if ~iscell(filenames)
     filenames = {filenames};
 end
    
-%%
 data(1) = load_fcs_data(pathname, filenames{1}, path_out);
 
 for i=2:length(filenames)
     data(i) = load_fcs_data(pathname, filenames{i}, path_out, data(1).roi_position);
-
 end
 
 %%
-i=14;
+i_fl_ch=14;
 sample_names = cell(length(filenames),1);
 
 for j=1:length(filenames)
-        sample_names{j} = [ filenames{j}(12:end-4) ' ' data(j).fcshdr.par(i).name];
+        sample_names{j} = [ filenames{j}(12:end-4) ' ' data(j).fcshdr.par(i_fl_ch).name];
 end
 
 %% Get limits
@@ -35,14 +31,13 @@ i = 14; % FL5-A
 
 tmp = [];
 for j=1:length(filenames)
-    tmp = [tmp; data(j).fcsdat(data(j).i_gated,i)];
+    tmp = [tmp; data(j).fcsdat(data(j).i_gated,i_fl_ch)];
 end
 xlim = [min(tmp) max(tmp)];
 xlim2 = [min(tmp)  median(tmp)+3*std(tmp)];
 xlim3 = [min(tmp) prctile(tmp, 98)];
 
 %% PLot FL5-A channel histogram, log scale
-
 cur_fig = figure(1); clf
 set(gcf,'Visible','on', 'PaperPositionMode', 'manual','PaperUnits','centimeters', ...
     'PaperPosition', [0 0 20 10 ], 'PaperSize', [20 10] );
@@ -52,17 +47,18 @@ for j=1:length(filenames)
     tmp = data(j).fcsdat(data(j).i_gated,i);
     
    
-    x=logspace(-1,5,100); % create bin edges with logarithmic scale
+    x=logspace(-1,8,100); % create bin edges with logarithmic scale
     histogram(tmp, x), hold on %, 'Normalization', 'pdf'
     
-    legend_tmp = [legend_tmp; {[ filenames{j}(12:end-4) ' ' data(j).fcshdr.par(i).name]}];
+    legend_tmp = [legend_tmp; {[ filenames{j}(12:end-4) ' ' data(j).fcshdr.par(i_fl_ch).name]}];
     disp([filenames{j} ', ' num2str(median(tmp))])
+    
 end
 legend(legend_tmp, 'Location', 'best')
 set(gca, 'xscale','log', 'xlim', [0 xlim(2)])
 xlabel(data(1).fcshdr.par(i).name), ylabel('Number of cells')
 
-print(cur_fig, '-dpdf', [path_out filesep prefix_out '_histogram_log.pdf']); %save figure
+print(cur_fig, '-dpdf', [path_out filesep prefix_out '_histogram_log_FL.pdf']); %save figure
 
 %% PLot FL5-A channel histogram
 
@@ -77,7 +73,7 @@ dx=20;
 
 legend_tmp = {};
 for j=1:length(filenames)
-    tmp = data(j).fcsdat(data(j).i_gated,i);
+    tmp = data(j).fcsdat(data(j).i_gated,i_fl_ch);
     
    
     %x=logspace(-1,5,100); % create bin edges with logarithmic scale
@@ -85,18 +81,18 @@ for j=1:length(filenames)
     [n, p, x_points] = uniform_kernel_density( tmp, h, x_start, x_stop, dx);
     plot(x_points, p), hold on
     
-    legend_tmp = [legend_tmp; {[ filenames{j}(12:end-4) ' ' data(j).fcshdr.par(i).name]}];
+    legend_tmp = [legend_tmp; {[ filenames{j}(12:end-4) ' ' data(j).fcshdr.par(i_fl_ch).name]}];
     disp([filenames{j} ', ' num2str(median(tmp))])
+    set(gca,  'xlim', xlim3)
+    pause
 end
 legend(legend_tmp, 'Location', 'best')
-set(gca,  'xlim', xlim3)
-xlabel(data(1).fcshdr.par(i).name), ylabel('Probability density')
+xlabel(data(1).fcshdr.par(i_fl_ch).name), ylabel('Probability density')
 
 print(cur_fig, '-dpdf', [path_out filesep prefix_out '_histogram_lin.pdf']); %save figure
 
 %% plot bar graph
 
-i = 14;
 cur_fig = figure(3); clf
 set(gcf,'Visible','on', 'PaperPositionMode', 'manual','PaperUnits','centimeters', ...
     'PaperPosition', [0 0 10 10 ], 'PaperSize', [10 10] );
@@ -107,9 +103,9 @@ val_mean = zeros(N_sample,1);
 val_err = zeros(N_sample,1);
 
 for j=1:length(filenames)
-    val_median(j) = median(data(j).fcsdat(data(j).i_gated,i));    
-    val_mean(j) = mean(data(j).fcsdat(data(j).i_gated,i));    
-    val_err(j) = 3*std(data(j).fcsdat(data(j).i_gated,i))/sqrt(length(data(j).fcsdat(data(j).i_gated,i)));    
+    val_median(j) = median(data(j).fcsdat(data(j).i_gated,i_fl_ch));    
+    val_mean(j) = mean(data(j).fcsdat(data(j).i_gated,i_fl_ch));    
+    val_err(j) = 3*std(data(j).fcsdat(data(j).i_gated,i_fl_ch))/sqrt(length(data(j).fcsdat(data(j).i_gated,i_fl_ch)));    
     
 end
 
@@ -119,17 +115,17 @@ bar(1:N_sample,val_median), hold on
 set(gca, 'Xtick', 1:N_sample, 'XTickLabel', sample_names)
 xtickangle(45)
 ylabel('Median Fluorescence')
+%set(gca,  'ylim', [0 1.5e4 ])%1.1*max(val_median)])
 set(gca,  'ylim', [0 1.1*max(val_median)])
 grid on
 
-print(cur_fig, '-dpdf', [path_out filesep prefix_out '_median.pdf']); %save figure
+print(cur_fig, '-dpdf', [path_out filesep prefix_out '_median2.pdf']); %save figure
 
 
 
 %% export csv
 file_out = [path_out prefix_out '_data.txt'];
 fileID = fopen(file_out,'w');
-i=14;
 
 fprintf(fileID,'Name\t');
 fprintf(fileID,'Median\t');
@@ -138,8 +134,8 @@ fprintf(fileID,'\n');
 
 for j=1:length(filenames)
     fprintf(fileID,'%s\t',sample_names{j});
-    fprintf(fileID,'%f\t', median(data(j).fcsdat(data(j).i_gated,i)));
-    fprintf(fileID,'%f\t', mean(data(j).fcsdat(data(j).i_gated,i)));
+    fprintf(fileID,'%f\t', median(data(j).fcsdat(data(j).i_gated,i_fl_ch)));
+    fprintf(fileID,'%f\t', mean(data(j).fcsdat(data(j).i_gated,i_fl_ch)));
     fprintf(fileID,'\n');
 end
 fclose(fileID);
@@ -148,17 +144,25 @@ fclose(fileID);
 save([path_out prefix_out '_data.mat'])
 
 %%
-t= [1 3]; %h
+t= [0.5 1 3]; %h
 
-
+tmp = parula();
+cc = tmp(32:64:end,:);%varycolor(3);
 
 cur_fig = figure(4); clf
 
-
-subplot(1, 2, 1)
+j_color = 1;
 myleg = cell(0,1);
-for i=1:4:length(val_median)
-    plot(t, val_median(i:i+1), '.-'), hold on
+for i=[1 4 7 10]
+    plot(t, val_median(i:i+2), '.-', 'Color', cc(j_color,:)), hold on
+    j_color = j_color +1;
+    myleg = [myleg, filenames{i}(12:end-7) ]
+end
+
+j_color = 1;
+for i=[13 16 19 22]
+    plot(t, val_median(i:i+2), '.--', 'Color', cc(j_color,:)), hold on
+    j_color = j_color +1;
     myleg = [myleg, filenames{i}(12:end-7) ]
 end
 grid on
@@ -167,21 +171,9 @@ legend(myleg, 'location', 'best')
 xlabel('Time (h)'), ylabel('Median Fluorescence')
     
 
-subplot(1, 2, 2)
-myleg = cell(0,1);
-for i=3:4:length(val_median)
-    plot(t, val_median(i:i+1), '.-'), hold on
-    myleg = [myleg, filenames{i}(12:end-7) ]
-end
-grid on
-set(gca, 'YLim', [0 1.1*max(val_median)], 'XLim', [0 t(end)+1])
-legend(myleg, 'location', 'best')
-xlabel('Time (h)'), ylabel('Median Fluorescence')
-
-
 set(gcf,'Visible','on', 'PaperPositionMode', 'manual','PaperUnits','centimeters', ...
     'PaperPosition', [0 0 30 20 ], 'PaperSize', [30 20] );
-print(cur_fig, '-dpdf', [path_out filesep prefix_out '_LGv5.pdf']); %save figure
+print(cur_fig, '-dpdf', [path_out filesep prefix_out '_vs_t.pdf']); %save figure
 
 
 %% Plot histogram for all channels
@@ -221,9 +213,10 @@ i=2; j=4; k=14; %FSC-A vs SSC-A vs. FL5-A
 
 cur_fig = figure(6); clf
 
-for l=1:2:length(filenames)
+for l=1:3:length(filenames)
     scatter3(data(l).fcsdat(:,i), data(l).fcsdat(:,j), data(l).fcsdat(:,k), 1, '.'), hold on
     scatter3(data(l+1).fcsdat(:,i), data(l+1).fcsdat(:,j), data(l+1).fcsdat(:,k), 1, '.')
+    scatter3(data(l+2).fcsdat(:,i), data(l+2).fcsdat(:,j), data(l+2).fcsdat(:,k), 1, '.')
     set(gca, 'xscale','log', 'yscale','log')
     set(gca, 'zscale','log')
     xlabel(data(l).fcshdr.par(i).name)
