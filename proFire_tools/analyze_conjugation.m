be430c2c81fb64bb004ec63c8d4fa10f47c13e1e
jonasfunke <jonasfunke@gmail.com>
2020-01-08 13:43:59 +0100

    profire fixes

--- proFire_tools/analyze_conjugation.m
+++ proFire_tools/analyze_conjugation.m
@@ -9,21 +9,71 @@ data = read_profire_csv();
 
 data_rate = 10*60; % data points/min
 e = 363294; % IgG-DNA
-data_rate_valve = 0.5*60; %data points / min
+%e = 136700 + 11710; % IL2 short modifier + extinction at 280
+e = 225800+ 11710; % IL2 long modifier + extinction at 280
 
-% custom_BL_IgG_collect
-%fraction_times = [3.0:0.6:4.8 8.6:0.6:14];
-%fraction = [1 2 3 13 4 5 6 7 8 9 10 11 12 ];
+data_rate_valve = 0.5*60; %data points / min
+%%
+
+% e_IgG = 141894;
+% e_7020 = 221400*15/26;
+% e = e_IgG + e_7020;
+
+% custom-BK-IgG-26_collect
+% fraction_times = [3.0:0.6:4.8 8.6:0.6:14];
+% fraction = [1 2 3 13 4 5 6 7 8 9 10 11 12 ];
+
+% custom-IL2-short
+% fraction_times = [11.4:0.6:18.6];
+% fraction = [1:12];
+
+% custom-IL2-longv2
+fraction_times = [8.4:0.6:15.6];
+fraction = [1:12];
 
 % custom_49_IgG-collect
 %fraction_times = [2.55 3.2 3.8 4.4 9.72:0.66:15.66];
 %fraction = [1 2 3 13 4 5 6 7 8 9 10 11 12 ];
 
 % custom-BK-IgG-26
-fraction_times = [6.8 7.4 8.0 8.6 9.2 9.8 10.4 11 11.6 12.2 12.8 13.4 14];
-fraction = [1 2 3 4 5 6 7 8 9 10 11 12 ];
+% fraction_times = [6.8 7.4 8.0 8.6 9.2 9.8 10.4 11 11.6 12.2 12.8 13.4 14];
+% fraction = [1 2 3 4 5 6 7 8 9 10 11 12 ];
+
+%proFIRE19_32-41bases
+% fraction_times = [7.2:0.65:15];
+% fraction = [1 2 3 4 5 6 7 8 9 10 11 12 ];
+
+%proFIRE16_29-31bases
+% fraction_times = [6.0:0.6:13.2];
+% fraction = [1 2 3 4 5 6 7 8 9 10 11 12 ];
+
+%proFIRE10_15-19bases
+% fraction_times = [6.0:0.6:13.2];
+% fraction = [1 2 3 4 5 6 7 8 9 10 11 12 ];
+
+% custom-BK-IgG-29-31
+% fraction_times = [7.1:0.6:14.3];
+% fraction = [1 2 3 4 5 6 7 8 9 10 11 12 ];
 
 
+%% determine fraction_times and fractions
+%  figure(1), clf
+% tmp = 1:length(data.Position_numbers);
+% % 
+% % plot( data.Position_numbers)
+% plot(tmp/data_rate_valve, data.Position_numbers)
+% 
+% fraction = unique(data.Position_numbers);
+% fraction = fraction(1:end-1); %remove fraction 13
+% fraction_times = zeros(1, length(fraction)+1);
+% for j=fraction
+%     index = tmp(data.Position_numbers==j); % index of fractions
+%     selection = index-index(1)-cumsum(ones(1,length(index)))+1; % select only thefirst fraction
+%     index = index(selection==0);
+%     fraction_times(j) = (index(1)-1)/data_rate_valve;
+% end
+% fraction_times(end) = (index(end))/data_rate_valve
+% fraction
 %%
 
 cur_fig = figure('Visible','on', 'PaperPositionMode', 'manual','PaperUnits','points','PaperPosition', [0 0 3000 600], 'Position', [0 1000 1000 500]);
@@ -31,7 +81,7 @@ clf
 plot([1:length(data.Signal_numbers)]/data_rate, data.Signal_numbers), hold on
 
 for j=1:length(fraction_times)-1
-    vline(fraction_times(j), 'k:');
+    vline(fraction_times(j), 'k-');
     
     index(1) = round(data_rate*fraction_times(j));
     index(2) = round(data_rate*fraction_times(j+1));
@@ -48,9 +98,9 @@ for j=1:length(fraction_times)-1
     text(fraction_times(j)+0.1*(fraction_times(j+1)-fraction_times(j)), 0.05, { ['F' num2str(fraction(j))],[num2str(round(concentration)) ' nM'], [num2str(round(amount)) ' pmol']})
 
 end
-vline(fraction_times(length(fraction_times)), 'k:');
-title(['Using 363294/M/cm extinction coefficient'])
-set(gca, 'XTick', 0:3:27, 'XLim', [0 30])%, 'YLim', [-10 130])
+vline(fraction_times(length(fraction_times)), 'k-');
+title(['Using ' num2str(e) ' /M/cm extinction coefficient'])
+%set(gca, 'XTick', 0:3:27, 'XLim', [0 30], 'YLim', [-1 20])
 grid on
 print(cur_fig, '-dpng', [data.pathname data.filename(1:end-4) '_analysis.png']); %save figure
 
--- proFire_tools/compare_conjugations.m
+++ proFire_tools/compare_conjugations.m
@@ -2,7 +2,7 @@
 close all, clear all, clc
 
 %% select csv file and read it
-n_data = 5;
+n_data = 4;
 
 data = cell(n_data,1);
 for i=1:n_data
@@ -10,12 +10,15 @@ for i=1:n_data
     %cd(data{i}.pathname)
 end
 %% set manually
-
 data_rate = 10*60; % data points/min
-%e = 363294; % IgG-DNA
-%data_rate_valve = 0.5*60; %data points / min
+e = 363294; % IgG-DNA
+data_rate_valve = 0.5*60; %data points / min
+
 %fraction_times = [6.8 7.4 8.0 8.6 9.2 9.8 10.4 11 11.6 12.2 12.8 13.4 14];
 %fraction = [1 2 3 4 5 6 7 8 9 10 11 12 ];
+
+fraction_times = [7.1:0.6:14.3];
+fraction = [1 2 3 4 5 6 7 8 9 10 11 12 ];
 
 %%
 cur_fig = figure('Visible','on', 'PaperPositionMode', 'manual','PaperUnits','points','PaperPosition', [0 0 3000 600], 'Position', [0 1000 1000 500]);
@@ -37,23 +40,31 @@ grid on
 %print(cur_fig, '-dpng', [data.pathname data.filename(1:end-4) '_analysis.png']); %save figure
 
 %%
-cur_fig = figure('Visible','on', 'PaperPositionMode', 'manual','PaperUnits','points','PaperPosition', [0 0 3000 600], 'Position', [0 1000 1000 500]);
-clf
+
+path_out = '/Users/jonasfunke/Dropbox (Personal)/Plectonic_Experiments/data_proFIRE/2019-11-08_CD33-7262-DNA-conc-screen';
+prefix_out = '2019-11-08_CD33-7262-DNA-conc-screen';
+cur_fig = figure(1); clf
+
 
 names = cell(n_data,1);
 for i=1:n_data
     plot([1:length(data{i}.Signal_numbers)]/data_rate, data{i}.Signal_numbers/abs(sum(data{i}.Signal_numbers))), hold on
     names{i} = data{i}.filename(1:end-4);
 end
-legend(names)
-
+tmp = {'35 uM DNA', '25 uM DNA', '15 uM DNA', '5 uM DNA'};
+legend(tmp, 'Interpreter', 'None')
+title('CD33 with 7262, 1.1 ug/ul CD33')
 xticks = 0:3:30;
-
-set(gca, 'XTick', xticks, 'XLim', [0 30])
+xlabel('Time'), ylabel('Absorption (a.U.)')
+set(gca, 'XTick', xticks, 'XLim', [2 18])
 grid on
 %print(cur_fig, '-dpng', [data.pathname data.filename(1:end-4) '_analysis.png']); %save figure
 
 
+
+set(gcf,'Visible','on', 'PaperPositionMode', 'manual','PaperUnits','centimeters', ...
+    'PaperPosition', [0 0 30 20 ], 'PaperSize', [30 20] );
+print(cur_fig, '-dpdf', [path_out filesep prefix_out '_2.pdf']); %save figure
 
 
 
