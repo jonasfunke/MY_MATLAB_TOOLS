6c33d188ef2cedb1e0ea9e53d1ee31959b2d31f8
jonasfunke <jonasfunke@gmail.com>
2020-01-10 14:42:26 +0100

    rename file for flow cytometry mesaurements

--- flow_cytometry/rename_files.m
+++ flow_cytometry/rename_files.m
@@ -6,25 +6,32 @@ close all, clear all, clc
 %path_in = '/Users/jonasfunke/Dropbox (Personal)/PlectonicBiotech/Experiments/data_FACS/2019-12-03-Facs/Positioning/';
 %path_in = '/Users/jonasfunke/Dropbox (Personal)/PlectonicBiotech/Experiments/data_FACS/2019-12-04_LGv7_ConfigurationScreen/'
 
-path_in = '/Users/jonasfunke/Dropbox (Personal)/PlectonicBiotech/Experiments/data_FACS/2019-08-28_PositioningScreen/Data/'
+path_in = '/Users/jonasfunke/Dropbox (Personal)/PlectonicBiotech/Experiments/data_FACS/2020-01-07-Concentration-Screen/NALM-CB-CD19/'
 
 files = dir([path_in '*.fcs']);
 
 %%
 
-for i=1:length(files)-5
+for i=1:length(files)-1
     
   
-    parts = split(files(i).name(1:end-4), ["-", "+"]);
-    index = parts{end};
+    [parts, matches] = split(files(i).name(1:end-4), ["-", "+"]);
+    %index = parts{end};
+    %time = parts{end-1};
     
 %     if strcmp(index, "0")
 %         name = 'CBonly';
 %     else
 %         name = index;
 %     end
-    name = index;
-    new_name = [ name '-' files(i).name(12:end)];
+    new_name = parts{1};
+    for j=2:length(parts)-2
+        new_name = [new_name matches{j-1} parts{j}];
+    end
+    new_name = [new_name '-' parts{end} '-' parts{end-1} '.fcs'];
+        
+    %name = index;
+    %new_name = [ name '-' files(i).name(12:end)];
     file_in = [files(i).folder filesep files(i).name];
     mkdir([files(i).folder filesep 'renamed'])
     file_out = [files(i).folder filesep 'renamed' filesep new_name];
