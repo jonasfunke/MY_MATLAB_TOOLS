e8469c8dede53ed00cb72ab79c2709b97689648e
jonasfunke <jonasfunke@gmail.com>
2020-03-29 13:18:26 +0200

    compute confidence interval for density estimations

--- general_functions/normal_kernel_density.m
+++ general_functions/normal_kernel_density.m
@@ -1,4 +1,4 @@
-function [n, p, x_points] = normal_kernel_density(x_data, h, x_start, x_stop, dx)
+function [n, p, x_points, ci] = normal_kernel_density(x_data, h, x_start, x_stop, dx)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -11,6 +11,20 @@ function [n, p, x_points] = normal_kernel_density(x_data, h, x_start, x_stop, dx
     end
     
     p = n/length(x_data);
+    
+    
+    
+    % estimate errors using wilson score interval (this is the improved method) for binomial distribution confidence
+    % interval 
+    p_tmp =  p*h; % probability
+    N = length(x_data); % number of total data points
+    z = 1-0.05/2; % confidence interval 95%
+    p_tmp_estimate = (p_tmp+z^2./2./N) ./ (1+z^2./N);
+    p_tmp_err = (z./(1+z^2./N)).*sqrt( p_tmp.*(1-p_tmp)./N+z^2./4./N.^2 );
+    ci = [p_tmp_estimate-p_tmp_err;  p_tmp_estimate+p_tmp_err]/h; % confidence interval of probabilty density
+    
+    
+    
 %     x_hist = x_start:h:x_stop;
 %     n_hist = hist(x_data, x_hist);
 %     
--- general_functions/uniform_kernel_density.m
+++ general_functions/uniform_kernel_density.m
@@ -1,4 +1,4 @@
-function [n, p, x_points] = uniform_kernel_density(x_data, h, x_start, x_stop, dx)
+function [n, p, x_points, ci] = uniform_kernel_density(x_data, h, x_start, x_stop, dx)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -13,8 +13,16 @@ function [n, p, x_points] = uniform_kernel_density(x_data, h, x_start, x_stop, d
 %             end
 %         end
     end
-    p = n/length(x_data);
+    p = n/length(x_data); % probability density
     
+    % estimate errors using wilson score interval (this is the improved method) for binomial distribution confidence
+    % interval 
+    p_tmp =  p*h; % probability
+    N = length(x_data); % number of total data points
+     z = 1-0.05/2; % confidence interval 95%
+    p_tmp_estimate = (p_tmp+z^2./2./N) ./ (1+z^2./N);
+    p_tmp_err = (z./(1+z^2./N)).*sqrt( p_tmp.*(1-p_tmp)./N+z^2./4./N.^2 );
+    ci = [p_tmp_estimate-p_tmp_err;  p_tmp_estimate+p_tmp_err]/h; % error in probabilty density
     
 %     x_hist = x_start:h:x_stop;
 %     n_hist = hist(x_data, x_hist);  
